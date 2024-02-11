ad73de7c2dfa5840a0056fd1dc1175da230f49c0
Ethan Luts <ethanl@4thdownsoftware.com>
2024-02-11 15:02:23 -0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 mod snapshot_utils;
-use crate::snapshot_utils::fetch_existing_snapshots_as_array_test;
-
+use crate::snapshot_utils::process_snapshot;
 use aws_sdk_ec2::Client;
+//use aws_smithy_types::date_time::DateTime;
+
+// Fucking retarted
+//fn parse_rfc3339_to_datetime(datetime_str: &str) -> DateTime {
+//    // Parse the RFC 3339 formatted string into the components
+//    let datetime_parts: Vec<&str> = datetime_str.split('T').collect();
+//    let date_parts: Vec<&str> = datetime_parts[0].split('-').collect();
+//    let time_parts: Vec<&str> = datetime_parts[1].split(':').collect();
+//
+//    //Extract the individual components
+//    let year: u64 = date_parts[0].parse().expect("Invalid year");
+//    let month: u64 = date_parts[1].parse().expect("Invalid month");
+//    let day: u64 = date_parts[2]
+//        .split('T')
+//        .next()
+//        .unwrap()
+//        .parse()
+//        .expect("Invalid day");
+//    let hour: u64 = time_parts[0].parse().expect("Invalid hour");
+//    let minute: u64 = time_parts[1].parse().expect("Invalid minute");
+//    let second: u64 = time_parts[2]
+//        .split('.')
+//        .next()
+//        .unwrap()
+//        .parse()
+//        .expect("Invalid second");
+//
+//    // Create the DateTime object
+//    DateTime::from_secs(second.try_into().unwrap())
+//}
 
 #[tokio::main]
 async fn main() {
@@ -16,5 +45,18 @@ async fn main() {
 
     let client = Client::new(&shared_config);
 
-    fetch_existing_snapshots_as_array_test(&client).await;
+    // Perform the request
+    let response = client.describe_snapshots().owner_ids("self").send().await;
+    if let Err(err) = response {
+        eprintln!("Error describing snapshots: {:?}", err);
+        return;
+    }
+
+    // Process each snapshot
+    if let Some(snapshots) = response.unwrap().snapshots {
+        for snapshot in snapshots {
+            // Call process_snapshot with snapshot and shared_config
+            process_snapshot(&client, snapshot);
+        }
+    }
 }
--- src/snapshot_utils.rs
+++ src/snapshot_utils.rs
@@ -1,126 +1,32 @@
-//use crate::configs::load_config;
-use aws_sdk_ec2::{Client, Error};
-use aws_smithy_types_convert::date_time::DateTimeExt;
-use chrono::{DateTime, Duration, Utc};
+use aws_sdk_ec2::types::Snapshot;
+use aws_sdk_ec2::Client;
+use std::time::Instant;
 
-// snapshotd --delete-if-older-than 30
-//
+pub fn process_snapshot(_client: &Client, snapshot: Snapshot) {
+    let now = Instant::now();
+    print!("Current Time is: {:?}", now);
+    // Extract snapshot ID
+    let snapshot_id = match snapshot.snapshot_id {
+        Some(id) => id,
+        None => {
+            eprintln!("Snapshot ID not found");
+            return;
+        }
+    };
 
-#[derive(Debug)]
-struct TimeStamp {
-    start_time: Option<DateTime<Utc>>,
+    // Extract snapshot start time
+    let start_time = match snapshot.start_time {
+        Some(time) => time.as_secs_f64(),
+        None => {
+            eprintln!("Snapshot start time not found");
+            return;
+        }
+    };
+
+    println!("ID: {:?}, CreatedAt: {:?}", snapshot_id, start_time);
 }
 
-#[allow(dead_code)]
-pub async fn fetch_existing_snapshots_as_array_test(client: &Client) -> Result<(), Error> {
-    let resp = client.describe_snapshots().owner_ids("self").send().await?;
-    let snapshots = resp.snapshots();
-    let count = snapshots.len();
-
-    println!("Found {} snapshot(s)", count);
-    for snapshot in snapshots {
-        println!(
-            "ID: {}, CreateAt: {:?}",
-            snapshot.snapshot_id().unwrap_or_default(),
-            snapshot.start_time()
-        );
-    }
-
-    Ok(())
-}
-
-//#[allow(dead_code)]
-//pub async fn fetch_existing_snapshots_as_array() -> Result<Vec<(String, i64, String)>, Error> {
-//    let config = match load_config().await {
-//        Ok(config) => config,
-//        Err(e) => {
-//            eprintln!("Error loading AWS SDK configuration: {:?}", e);
-//            return Err(e); // Convert the error type if necessary
-//        }
-//    };
-//
-//    let ec2 = Client::new(&config);
-//
-//    let resp = ec2.describe_snapshots().owner_ids("self").send().await?;
-//    let snapshots = resp.snapshots();
-//
-//    count_snapshots().await?;
-//
-//    let mut snapshot_ids = Vec::new();
-//
-//    for snapshot in snapshots {
-//        if let (Some(snapshot_id), Some(start_time)) =
-//            (snapshot.snapshot_id(), snapshot.start_time())
-//        {
-//            // Convert &str to String for easier manipulation
-//            let start_time_str = start_time.to_string();
-//            if let Ok(start_time_dt) = DateTime::parse_from_rfc3339(&start_time_str) {
-//                // Convert to UTC timezone
-//                let start_time_utc = start_time_dt.with_timezone(&Utc);
-//                // Convert to seconds since Unix epoch
-//                let start_time_seconds = start_time_utc.timestamp();
-//                snapshot_ids.push((snapshot_id.to_string(), start_time_seconds, start_time_str));
-//            } else {
-//                eprintln!("Error parsing start time: {}", start_time);
-//            }
-//        }
-//    }
-//
-//    Ok(snapshot_ids)
-//}
-
-//#[allow(dead_code)]
-//// should accept a number of days, and return the ids, that are older than the day provided.
-//// find a mechanism for mapping the IDs to timestamps
-//pub async fn older_than(_days: i64) -> Result<(), Error> {
-//    let dt = Utc::now();
-//    let timestamp: i64 = dt.timestamp();
-//    //eprintln!("Chrono UTC Timestamp: {:?}", timestamp);
-//
-//    let specified_time = Duration::days(30).num_seconds(); //bullshit
-//
-//    let older_than_thirty_days = timestamp - specified_time;
-//
-//    // Created At: 1707192484
-//    // Created At: 1707177621
-//    let snapshot_timestamps = fetch_existing_snapshots_timestamps().await?;
-//
-//    let mut snapshots_to_delete = Vec::new();
-//
-//    for timestamp in snapshot_timestamps {
-//        if timestamp > older_than_thirty_days {
-//            snapshots_to_delete.push(timestamp);
-//
-//            eprintln!(
-//                "The following are older than x days and will be delete: {:?}",
-//                snapshots_to_delete,
-//            );
-//        } else {
-//            println!("Not found");
-//        }
-//    }
-//    Ok(())
-//}
-//
-//#[allow(dead_code)]
-//// return the IDs, iterate, and finally delete them.
-//pub async fn delete_snapshots() -> Result<(), Error> {
-//    let config = match load_config().await {
-//        Ok(config) => config,
-//        Err(e) => {
-//            eprintln!("Error loading AWS SDK configuration: {:?}", e);
-//            return Err(e); // Convert the error type if necessary
-//        }
-//    };
-//
-//    let ec2 = Client::new(&config);
-//
-//    older_than(123213).await?;
-//    ec2.delete_snapshot();
-//    Ok(())
-//}
-//
-//pub async fn count_snapshots() -> Result<(), Error> {
+//pub async fn count_snapshots() -> Result<(), Error> {k
 //    let config = match load_config().await {
 //        Ok(config) => config,
 //        Err(e) => {
